7bb3a61d24bc4400a5e41035a7465197072e3698
Gabriel Fernandez <gabito2005usa@gmail.com>
2024-01-12 15:12:20 -0300

    clase 6

--- index.js
+++ index.js
@@ -3,40 +3,62 @@ const app = express()
 const PORT=8080
 const ProductManager= require('./segundaPreentrega')
 const productmanager= new ProductManager('products.JSON')
+
+//Bienvenida
 app.get('/bienvenida', (req,res)=>{
-    res.send('<h1 style="color: blue">Hola mundo</h1>')
+    res.send('<div><h1 style="color: blue">Hola mundo</h1></div>')
 })
 
-app.get('/products/:id', (req,res)=>{
-    const id=req.params.id
-    console.log(`el id por params es: ${id}`)
-    const product= productmanager.getProductById(id)
-    res.send(` <h1 style="color: blue">Hola mundo</h1> <h2>Producto ${id}: ${product}</h2>`)
-})
-
+//Todos los productos, URL: http://localhost:8080/products
 app.get('/products', (req,res)=>{
     const productos=productmanager.getProducts()
-    res.send(`<h1 style="color: blue">Hola mundo</h1> <h2 style="color: red">${productos}</h2>\n `)
+    res.send(`
+    <div>
+        <h1 style="color: red">
+            Todos los productos
+        </h1>
+        <h2 style="color: blue">
+            ${productos}
+        </h2>
+    </div> `)
 })
+
+//Producto por su id, URL: http://localhost:8080/products/...
+app.get('/products/:id', (req,res)=>{
+    const id=Number(req.params.id)//lo convierte a Number para usarlo en getProductById
+    const product= productmanager.getProductById(id)
+    res.send(`
+    <div>
+        <h1 style="color: red">
+            Producto con ID: ${id}
+        </h1>
+        <h2>
+            ${product}
+        </h2>
+    </div>`)
+})
+
+//Mostrar un limite de productos, URL: http://localhost:8080/products?limit=...
+app.get('/productsLimit', (req,res)=>{
+        let limit= Number(req.query.limit)//lo convierte a Number para usarlo en getProductById
+        let productsToSend=[]
+        for (let index = 0; index < limit; index++) {
+            const element = productmanager.getProductById(index+1)
+            productsToSend.push(element)
+        }
+        res.send(`
+        <div>
+            <h1 style="color:red">
+                Cantidad de productos: ${limit}
+            </h1>
+            <h2 style="color:green">
+                ${productsToSend}
+            </h2>
+        </div>
+        `)
+})
+
 
 app.listen(PORT,()=>{
     console.log('listening on port ' + PORT)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- segundaPreentrega.js
+++ segundaPreentrega.js
@@ -14,10 +14,10 @@ class ProductManager {
             if (!this.producto.some((product)=>product.code===code)) {
                 //generar ID
                 let idAutoincrementable=this.producto.length +1
-
                 let newProduct={title, description, price, thumbnail, code, stock, idAutoincrementable}
                 this.producto.push(newProduct)
                 console.log(`-Libro añadido: ${title}, el id de este producto es ${newProduct.idAutoincrementable}`)
+                this.saveProducts()
             }
             else{
                 console.log(`el producto con el codigo: ${code} ya fue ingresado`)
@@ -31,7 +31,7 @@ class ProductManager {
     getProducts(){
         if (this.producto.length) {
             let oldProducts= this.producto.map(product=>{
-                return ` \n ID: ${product.idAutoincrementable}, Title: ${product.title}, Price: ${product.price}`
+                return `ID: ${product.idAutoincrementable}, Title: ${product.title}, Price: ${product.price}`
             })
             return oldProducts
         }
@@ -41,9 +41,8 @@ class ProductManager {
     }
 
     getProductById(id){
-        let productById=this.producto.find((producto)=>producto.idAutoincrementable==id)
+        let productById=this.producto.find((producto)=>producto.idAutoincrementable===id)
         if(productById){
-            
             return JSON.stringify(productById)
         }
         if(!productById){
@@ -94,25 +93,44 @@ class ProductManager {
 module.exports = ProductManager
 
 const products = new ProductManager('products.JSON')
+
 //añadir productos
 products.addProduct('el rey leon', 'un libro de reyes leones', 600, 'www.elrey.com', 123, 8)
 products.addProduct('la bella y la besta', 'un libro de bella y bestia', 500, 'www.labestia.com', 456, 5)
+products.addProduct('Cien años de soledad','Obra maestra del realismo mágico escrita por Gabriel García Márquez.',30,'www.libro1.com',124,50)
+products.addProduct('1984','Distopía escrita por George Orwell que describe un futuro totalitario.',22,'www.libro2.com',454,30)
+products.addProduct('El Hobbit','Aventuras de Bilbo Bolsón escritas por J.R.R. Tolkien.',18,'www.libro3.com',789,40)
+products.addProduct('Matar a un ruiseñor','Novela clásica escrita por Harper Lee sobre injusticia racial.',35,'www.libro4.com',234,20)
+products.addProduct('Don Quijote de la Mancha','Obra maestra de la literatura española escrita por Miguel de Cervantes.',28,'www.libro5.com',567,45)
+products.addProduct('Orgullo y prejuicio','Clásico de Jane Austen que aborda temas de amor y clase social.',15,'www.libro6.com',890,60)
+products.addProduct('Crónica de una muerte anunciada','Relato de Gabriel García Márquez sobre un asesinato premeditado.',42,'www.libro7.com',345,25)
+products.addProduct('Harry Potter y la piedra filosofal','Primera entrega de la famosa serie escrita por J.K. Rowling.',20,'www.libro8.com',678,35)
+products.addProduct('Sapiens: De animales a dioses','Historia de la humanidad escrita por Yuval Noah Harari.',25,'www.libro9.com',901,55)
+products.addProduct('El código Da Vinci','Thriller de misterio escrito por Dan Brown.',32,'www.libro10.com',125,48)
 
 console.log('-------------------------------------------------------')
 
-// //obtener todos los productos
-// products.getProducts()
-// console.log('-------------------------------------------------------')
-// //obtener producto por el ID
-// products.getProductById(2)
-// // Ejemplo de uso de los nuevos métodos
-// console.log('-------------------------------------------------------')
-// products.updateProduct(1, { price: 700, stock: 10 }); // Actualizar precio y stock del producto con ID 1
-// console.log('-------------------------------------------------------')
-// // products.deleteProduct(2); // Eliminar el producto con ID 2
-// // products.deleteProduct(1); // Eliminar el producto con ID 2
-// console.log('-------------------------------------------------------')
-// products.getProducts(); // Mostrar todos los productos después de las operaciones
+//obtener todos los productos
+products.getProducts()
+
+console.log('-------------------------------------------------------')
+
+//obtener producto por el ID
+products.getProductById(2)
+
+console.log('-------------------------------------------------------')
+
+// Actualizar precio y stock del producto con ID 1
+products.updateProduct(1, { price: 700, stock: 10 })
+
+console.log('-------------------------------------------------------')
+
+// Eliminar el producto con ID
+// products.deleteProduct(2); // Eliminar el producto con ID 2
+//console.log('-------------------------------------------------------')
+
+// Mostrar todos los productos después de las operaciones
+products.getProducts() 
 
 
 
